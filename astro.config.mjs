cda037ed91276fc06d46d1dcf7b98d9131bfcdc2
Nuno Antunes <nunofcg.antunes@gmail.com>
2024-03-11 01:01:18 +0000

    aa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 import { defineConfig } from "astro/config";
+import astroI18next from "astro-i18next";
+import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-naut-template.vercel.app/",
+  integrations: [astroI18next(), tailwind({
+    applyBaseStyles: false,
+  }), react()],
+  site: "https://astro-naut-template.vercel.app/"
 });
